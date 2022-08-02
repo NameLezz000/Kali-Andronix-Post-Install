15d7d573ec147cec1b2075d6e66875911e7a7fff
Eins NameLezz <67716134+NameLezz000@users.noreply.github.com>
2022-08-02 17:17:35 -0300

    Update run.sh
    Added more automation and some cleanup

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+apt update
+apt install gnupg -y
+
+mkdir /etc/apt/sources.list.d
+touch kali.list
+sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
+
+wget 'https://archive.kali.org/archive-key.asc'
+apt-key add archive-key.asc
+
 apt update && apt upgrade -y
 
 apt install -y kali-desktop-xfce
 apt install -y tigervnc-standalone-server
 
-apt upgrade -y
+clear
+
+echo done
